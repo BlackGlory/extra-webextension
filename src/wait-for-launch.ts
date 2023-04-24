caf1510522bdd5564613d7f13364cdda56a5271b
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-04-24 03:31:48 +0000

    fix: waitForLaunch

--- src/wait-for-launch.ts
+++ src/wait-for-launch.ts
@@ -1,6 +1,7 @@
 import browser from 'webextension-polyfill'
-import { go, assert, pass } from '@blackglory/prelude'
+import { assert } from '@blackglory/prelude'
 import { SessionStorage } from './session-storage.js'
+import { timeout, TimeoutError } from 'extra-promise'
 
 export enum LaunchReason {
   Install
@@ -19,49 +20,8 @@ export type LaunchDetails =
 | { reason: LaunchReason.Activate }
 
 // 确保事件监听器在模块导入时就被注册.
-const installedPromise = go(() => {
-  const promise = new Promise<
-  | { reason: LaunchReason.Install }
-  | { reason: LaunchReason.Enable }
-  | {
-      reason: LaunchReason.Update
-      previousVersion: string
-    }
-  >((resolve, reject) => {
-    browser.runtime.onInstalled.addListener(async details => {
-      try {
-        switch (details.reason) {
-          case 'install': {
-            resolve({ reason: LaunchReason.Install })
-            break
-          }
-          case 'update': {
-            assert(
-              details.previousVersion
-            , 'The details.previousVersion is undefined, which is unexpected.'
-            )
-
-            resolve({
-              reason: LaunchReason.Update
-            , previousVersion: details.previousVersion
-            })
-            break
-          }
-          default: {
-            resolve({ reason: LaunchReason.Enable })
-            break
-          }
-        }
-      } catch (e) {
-        reject(e)
-      }
-    })
-  })
-
-  // 避免因未及时处理错误而抛出错误
-  promise.catch(pass)
-
-  return promise
+const installDetailsPromise = new Promise<browser.Runtime.OnInstalledDetailsType>(resolve => {
+  browser.runtime.onInstalled.addListener(resolve)
 })
 
 export async function waitForLaunch(): Promise<LaunchDetails> {
@@ -72,6 +32,38 @@ export async function waitForLaunch(): Promise<LaunchDetails> {
     return { reason: LaunchReason.Activate }
   } else {
     await storage.setItem(activeFlagKey, true)
-    return await installedPromise
+
+    try {
+      const details = await Promise.race([
+        installDetailsPromise // 在一般启动时, 该Promise永远不会完成.
+      , timeout(1000) // 如果超时, 说明是一般启动.
+      ])
+
+      switch (details.reason) {
+        case 'install': {
+          return { reason: LaunchReason.Install }
+        }
+        case 'update': {
+          assert(
+            details.previousVersion
+          , 'The details.previousVersion is undefined, which is unexpected.'
+          )
+
+          return {
+            reason: LaunchReason.Update
+          , previousVersion: details.previousVersion
+          }
+        }
+        default: {
+          return { reason: LaunchReason.Enable }
+        }
+      }
+    } catch (e) {
+      if (e instanceof TimeoutError) {
+        return { reason: LaunchReason.Enable }
+      } else {
+        throw e
+      }
+    }
   }
 }
